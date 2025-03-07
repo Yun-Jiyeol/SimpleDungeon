38f75ccac24f25470a8a13671a188a783283c259
Yun-Jiyeol <yir0612@gmail.com>
2025-03-07 17:58:39 +0900

    Feat: 점프보드에 시간 추가

--- Assets/Scripts/Board/JumpBoard.cs
+++ Assets/Scripts/Board/JumpBoard.cs
@@ -1,9 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class JumpBoard : MonoBehaviour
 {
     public float jumpForce;
+    public float jumpTime;
+
     private Animator animator;
+    private List<GameObject> Go = new List<GameObject>();
+    private Coroutine jump;
 
     private void Start()
     {
@@ -12,13 +18,25 @@ public class JumpBoard : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        animator.SetBool("IsSomethingOn",true);
-        collision.rigidbody.velocity = Vector3.zero;
-        collision.rigidbody.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
+        Go.Add(collision.gameObject);
+        jump = StartCoroutine(jumpcollider());
     }
 
     private void OnCollisionExit(Collision collision)
     {
+        Go.Remove(collision.gameObject);
+        StopCoroutine(jump);
         animator.SetBool("IsSomethingOn", false);
+    }
+
+    IEnumerator jumpcollider()
+    {
+        yield return new WaitForSeconds(jumpTime);
+        animator.SetBool("IsSomethingOn", true);
+        foreach(GameObject go in Go)
+        {
+            go.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            go.GetComponent<Rigidbody>().AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
